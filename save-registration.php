41d9c5f28bc3894e61bc48d4ba549fe8a25c10c6
JoaquinCorpuz <130562568+joaquincorpuz21@users.noreply.github.com>
2023-04-26 18:45:34 +0800

    Add files via upload

---
+++ save-registration.php
@@ -0,0 +1,27 @@
+<?php
+
+require "config.php";
+
+use App\User;
+
+// Save the user information, and automatically login the user
+
+try {
+	$first_name = $_POST['first_name'];
+	$midde_name = $_POST['middle_name']
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$result = User::register($first_name, $middle_name, $last_name, $email, $password);
+	if ($result) {
+
+	// Set the logged in session variable and redirect user to index page
+
+	$_SESSION['is_logged_in'] = true;
+	$_SESSION['user'] = [
+		'id' => $result,
+		'fullname' => $first_name . ' ' . $last_name,
+		'email' => $email
+	];
+	header('Location: index.php');}
+};
